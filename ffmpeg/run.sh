11e2b6c94f9ca7321a41c1c94138a640ae250b51
zmad5306 <zmad5306@protonmail.com>
2021-01-30 23:43:46 +0000

    converting mp4 to mkv

--- ffmpeg/run.sh
+++ ffmpeg/run.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-for file in /vids/*.mp4
-do
-	fn=$(echo $file | cut -f 1 -d '.')
-    	ffmpeg -y -i $file -c copy $fn.mkv
+#!/bin/sh
+inotifywait -m --exclude "[^mp4]$" /vids -e close_write |
+    while read path action file; do
+        echo "The file '$file' appeared in directory '$path' via '$action'"
+        fn=$(echo $file | cut -f 1 -d '.')
+        echo "The filename is '$fn'"
+        ffmpeg -y -i "$path$file" -c copy "$path$fn.mkv"
+        rm "$path$file"
 done
